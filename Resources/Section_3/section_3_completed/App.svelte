84577d77412ba1687d9e144098f06fe07bea9bc9
ryanwk <ryanwkoch@gmail.com>
2020-11-23 13:43:52 -0500

    section 3 assignment 2 complete (minus bonus)

---
+++ Resources/Section_3/section_3_completed/App.svelte
@@ -0,0 +1,88 @@
+<script>
+  import ContactCard from "./ContactCard.svelte";
+
+  let name = "Ryan";
+  let title = "";
+  let image = "";
+  let description = "";
+  let formState = "empty";
+
+  let createdContacts = [];
+
+  function addContact() {
+    if (
+      name.trim().length == 0 ||
+      title.trim().length == 0 ||
+      image.trim().length == 0 ||
+      description.trim().length == 0
+    ) {
+      formState = "invalid";
+      return;
+    }
+    createdContacts = [
+      ...createdContacts,
+      {
+        id: Math.random(),
+        name: name,
+        jobTitle: title,
+        imgUrl: image,
+        desc: description,
+      },
+    ];
+    formState = "done";
+  }
+
+  function deleteFirst() {
+    createdContacts = createdContacts.slice(1);
+  }
+  function deleteLast() {
+    createdContacts = createdContacts.slice(0, -1);
+  }
+</script>
+
+<style>
+  #form {
+    width: 30rem;
+    max-width: 100%;
+  }
+</style>
+
+<div id="form">
+  <div class="form-control">
+    <label for="userName">User Name</label>
+    <input type="text" bind:value={name} id="userName" />
+  </div>
+  <div class="form-control">
+    <label for="jobTitle">Job Title</label>
+    <input type="text" bind:value={title} id="jobTitle" />
+  </div>
+  <div class="form-control">
+    <label for="image">Image URL</label>
+    <input type="text" bind:value={image} id="image" />
+  </div>
+  <div class="form-control">
+    <label for="desc">Description</label>
+    <textarea rows="3" bind:value={description} id="desc" />
+  </div>
+</div>
+
+<button on:click={addContact}>Add Contact Card</button>
+<button on:click={deleteFirst}>Delete First</button>
+<button on:click={deleteLast}>Delete Last</button>
+
+{#if formState === 'invalid'}
+  <p>Invalid input</p>
+{:else}
+  <p>Please complete the fields above then press the button</p>
+{/if}
+
+{#each createdContacts as contact, i (contact.id)}
+  <h1># {i + 1}</h1>
+  <ContactCard
+    userName={contact.name}
+    jobTitle={contact.jobTitle}
+    description={contact.desc}
+    userImage={contact.imageUrl} />
+{:else}
+  <p>Please start adding contacts, there currently are 0 contacts present.</p>
+{/each}
