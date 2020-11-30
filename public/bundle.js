fbee90f778091296ea3581436bc242ffa89745fc
ryanwk <ryanwkoch@gmail.com>
2020-11-30 15:34:47 -0500

    setting up course project 1
    created Meetups/
    created UI/
    
    app.svelte added header and preliminary css in the style brackets

--- public/bundle.js
+++ public/bundle.js
@@ -37,24 +37,8 @@ var app = (function () {
     function detach(node) {
         node.parentNode.removeChild(node);
     }
-    function destroy_each(iterations, detaching) {
-        for (let i = 0; i < iterations.length; i += 1) {
-            if (iterations[i])
-                iterations[i].d(detaching);
-        }
-    }
     function element(name) {
         return document.createElement(name);
-    }
-    function text(data) {
-        return document.createTextNode(data);
-    }
-    function space() {
-        return text(' ');
-    }
-    function listen(node, event, handler, options) {
-        node.addEventListener(event, handler, options);
-        return () => node.removeEventListener(event, handler, options);
     }
     function attr(node, attribute, value) {
         if (value == null)
@@ -64,9 +48,6 @@ var app = (function () {
     }
     function children(element) {
         return Array.from(element.childNodes);
-    }
-    function set_input_value(input, value) {
-        input.value = value == null ? '' : value;
     }
     function custom_event(type, detail) {
         const e = document.createEvent('CustomEvent');
@@ -283,41 +264,12 @@ var app = (function () {
         dispatch_dev('SvelteDOMRemove', { node });
         detach(node);
     }
-    function listen_dev(node, event, handler, options, has_prevent_default, has_stop_propagation) {
-        const modifiers = options === true ? ['capture'] : options ? Array.from(Object.keys(options)) : [];
-        if (has_prevent_default)
-            modifiers.push('preventDefault');
-        if (has_stop_propagation)
-            modifiers.push('stopPropagation');
-        dispatch_dev('SvelteDOMAddEventListener', { node, event, handler, modifiers });
-        const dispose = listen(node, event, handler, options);
-        return () => {
-            dispatch_dev('SvelteDOMRemoveEventListener', { node, event, handler, modifiers });
-            dispose();
-        };
-    }
     function attr_dev(node, attribute, value) {
         attr(node, attribute, value);
         if (value == null)
             dispatch_dev('SvelteDOMRemoveAttribute', { node, attribute });
         else
             dispatch_dev('SvelteDOMSetAttribute', { node, attribute, value });
-    }
-    function set_data_dev(text, data) {
-        data = '' + data;
-        if (text.wholeText === data)
-            return;
-        dispatch_dev('SvelteDOMSetData', { node: text, data });
-        text.data = data;
-    }
-    function validate_each_argument(arg) {
-        if (typeof arg !== 'string' && !(arg && typeof arg === 'object' && 'length' in arg)) {
-            let msg = '{#each} only iterates over array-like objects.';
-            if (typeof Symbol === 'function' && arg && Symbol.iterator in arg) {
-                msg += ' You can use a spread to convert this iterable into an array.';
-            }
-            throw new Error(msg);
-        }
     }
     function validate_slots(name, slot, keys) {
         for (const slot_key of Object.keys(slot)) {
@@ -347,363 +299,31 @@ var app = (function () {
 
     const file = "src/App.svelte";
 
-    function get_each_context(ctx, list, i) {
-    	const child_ctx = ctx.slice();
-    	child_ctx[6] = list[i];
-    	child_ctx[8] = i;
-    	return child_ctx;
-    }
-
-    // (67:0) {:else}
-    function create_else_block(ctx) {
-    	let p;
-    	let t0;
-    	let t1;
-
-    	const block = {
-    		c: function create() {
-    			p = element("p");
-    			t0 = text("Password: ");
-    			t1 = text(/*enteredPassword*/ ctx[0]);
-    			add_location(p, file, 67, 2, 1715);
-    		},
-    		m: function mount(target, anchor) {
-    			insert_dev(target, p, anchor);
-    			append_dev(p, t0);
-    			append_dev(p, t1);
-    		},
-    		p: function update(ctx, dirty) {
-    			if (dirty & /*enteredPassword*/ 1) set_data_dev(t1, /*enteredPassword*/ ctx[0]);
-    		},
-    		d: function destroy(detaching) {
-    			if (detaching) detach_dev(p);
-    		}
-    	};
-
-    	dispatch_dev("SvelteRegisterBlock", {
-    		block,
-    		id: create_else_block.name,
-    		type: "else",
-    		source: "(67:0) {:else}",
-    		ctx
-    	});
-
-    	return block;
-    }
-
-    // (65:39) 
-    function create_if_block_1(ctx) {
-    	let p;
-
-    	const block = {
-    		c: function create() {
-    			p = element("p");
-    			p.textContent = "Password is too short";
-    			add_location(p, file, 65, 2, 1676);
-    		},
-    		m: function mount(target, anchor) {
-    			insert_dev(target, p, anchor);
-    		},
-    		p: noop,
-    		d: function destroy(detaching) {
-    			if (detaching) detach_dev(p);
-    		}
-    	};
-
-    	dispatch_dev("SvelteRegisterBlock", {
-    		block,
-    		id: create_if_block_1.name,
-    		type: "if",
-    		source: "(65:39) ",
-    		ctx
-    	});
-
-    	return block;
-    }
-
-    // (63:0) {#if passwordValidity === 'long'}
-    function create_if_block(ctx) {
-    	let p;
-
-    	const block = {
-    		c: function create() {
-    			p = element("p");
-    			p.textContent = "Password is too long";
-    			add_location(p, file, 63, 2, 1606);
-    		},
-    		m: function mount(target, anchor) {
-    			insert_dev(target, p, anchor);
-    		},
-    		p: noop,
-    		d: function destroy(detaching) {
-    			if (detaching) detach_dev(p);
-    		}
-    	};
-
-    	dispatch_dev("SvelteRegisterBlock", {
-    		block,
-    		id: create_if_block.name,
-    		type: "if",
-    		source: "(63:0) {#if passwordValidity === 'long'}",
-    		ctx
-    	});
-
-    	return block;
-    }
-
-    // (72:2) {#each passwords as pw, i}
-    function create_each_block(ctx) {
-    	let li;
-    	let t_value = /*pw*/ ctx[6] + "";
-    	let t;
-    	let mounted;
-    	let dispose;
-
-    	const block = {
-    		c: function create() {
-    			li = element("li");
-    			t = text(t_value);
-    			add_location(li, file, 72, 4, 1795);
-    		},
-    		m: function mount(target, anchor) {
-    			insert_dev(target, li, anchor);
-    			append_dev(li, t);
-
-    			if (!mounted) {
-    				dispose = listen_dev(li, "click", /*removePassword*/ ctx[4].bind(this, /*i*/ ctx[8]), false, false, false);
-    				mounted = true;
-    			}
-    		},
-    		p: function update(new_ctx, dirty) {
-    			ctx = new_ctx;
-    			if (dirty & /*passwords*/ 4 && t_value !== (t_value = /*pw*/ ctx[6] + "")) set_data_dev(t, t_value);
-    		},
-    		d: function destroy(detaching) {
-    			if (detaching) detach_dev(li);
-    			mounted = false;
-    			dispose();
-    		}
-    	};
-
-    	dispatch_dev("SvelteRegisterBlock", {
-    		block,
-    		id: create_each_block.name,
-    		type: "each",
-    		source: "(72:2) {#each passwords as pw, i}",
-    		ctx
-    	});
-
-    	return block;
-    }
-
     function create_fragment(ctx) {
+    	let header;
     	let h1;
-    	let t1;
-    	let p;
-    	let t3;
-    	let ol;
-    	let li0;
-    	let t5;
-    	let li1;
-    	let t7;
-    	let li2;
-    	let t9;
-    	let li3;
-    	let t11;
-    	let li4;
-    	let t13;
-    	let li5;
-    	let t15;
-    	let div;
-    	let h3;
-    	let t17;
-    	let input;
-    	let button;
-    	let t19;
-    	let t20;
-    	let ul;
-    	let mounted;
-    	let dispose;
-
-    	function select_block_type(ctx, dirty) {
-    		if (/*passwordValidity*/ ctx[1] === "long") return create_if_block;
-    		if (/*passwordValidity*/ ctx[1] === "short") return create_if_block_1;
-    		return create_else_block;
-    	}
-
-    	let current_block_type = select_block_type(ctx);
-    	let if_block = current_block_type(ctx);
-    	let each_value = /*passwords*/ ctx[2];
-    	validate_each_argument(each_value);
-    	let each_blocks = [];
-
-    	for (let i = 0; i < each_value.length; i += 1) {
-    		each_blocks[i] = create_each_block(get_each_context(ctx, each_value, i));
-    	}
 
     	const block = {
     		c: function create() {
+    			header = element("header");
     			h1 = element("h1");
-    			h1.textContent = "Assignment";
-    			t1 = space();
-    			p = element("p");
-    			p.textContent = "Solve these tasks.";
-    			t3 = space();
-    			ol = element("ol");
-    			li0 = element("li");
-    			li0.textContent = "Add a enteredPassword input field and save the user input in a variable.";
-    			t5 = space();
-    			li1 = element("li");
-    			li1.textContent = "Output \"Too short\" if the password is shorter than 5 characters and \"Too\n    long\" if it's longer than 10.";
-    			t7 = space();
-    			li2 = element("li");
-    			li2.textContent = "Output the password in a paragraph tag (below the input or below the\n    warnings) if it's between these boundaries.";
-    			t9 = space();
-    			li3 = element("li");
-    			li3.textContent = "Add a button and let the user add the passwords to an array (an array that\n    grows with every password that was entered - not too long not too short).";
-    			t11 = space();
-    			li4 = element("li");
-    			li4.textContent = "Output the array values (= passwords) in a unordered list (ul tag).";
-    			t13 = space();
-    			li5 = element("li");
-    			li5.textContent = "Bonus: If a password is clicked, remove it from the list.";
-    			t15 = space();
-    			div = element("div");
-    			h3 = element("h3");
-    			h3.textContent = "Password";
-    			t17 = space();
-    			input = element("input");
-    			button = element("button");
-    			button.textContent = "Confirm Password";
-    			t19 = space();
-    			if_block.c();
-    			t20 = space();
-    			ul = element("ul");
-
-    			for (let i = 0; i < each_blocks.length; i += 1) {
-    				each_blocks[i].c();
-    			}
-
-    			add_location(h1, file, 30, 0, 628);
-    			add_location(p, file, 32, 0, 649);
-    			add_location(li0, file, 35, 2, 683);
-    			add_location(li1, file, 38, 2, 775);
-    			add_location(li2, file, 42, 2, 901);
-    			add_location(li3, file, 46, 2, 1037);
-    			add_location(li4, file, 50, 2, 1209);
-    			add_location(li5, file, 51, 2, 1288);
-    			add_location(ol, file, 34, 0, 676);
-    			add_location(h3, file, 55, 2, 1383);
-    			attr_dev(input, "type", "text");
-    			add_location(input, file, 56, 2, 1403);
-    			add_location(button, file, 56, 52, 1453);
-    			attr_dev(div, "class", "form");
-    			add_location(div, file, 54, 0, 1362);
-    			add_location(ul, file, 70, 0, 1757);
+    			h1.textContent = "Meet Ups";
+    			add_location(h1, file, 12, 2, 114);
+    			attr_dev(header, "class", "svelte-1ah6c1");
+    			add_location(header, file, 11, 0, 103);
     		},
     		l: function claim(nodes) {
     			throw new Error("options.hydrate only works if the component was compiled with the `hydratable: true` option");
     		},
     		m: function mount(target, anchor) {
-    			insert_dev(target, h1, anchor);
-    			insert_dev(target, t1, anchor);
-    			insert_dev(target, p, anchor);
-    			insert_dev(target, t3, anchor);
-    			insert_dev(target, ol, anchor);
-    			append_dev(ol, li0);
-    			append_dev(ol, t5);
-    			append_dev(ol, li1);
-    			append_dev(ol, t7);
-    			append_dev(ol, li2);
-    			append_dev(ol, t9);
-    			append_dev(ol, li3);
-    			append_dev(ol, t11);
-    			append_dev(ol, li4);
-    			append_dev(ol, t13);
-    			append_dev(ol, li5);
-    			insert_dev(target, t15, anchor);
-    			insert_dev(target, div, anchor);
-    			append_dev(div, h3);
-    			append_dev(div, t17);
-    			append_dev(div, input);
-    			set_input_value(input, /*enteredPassword*/ ctx[0]);
-    			append_dev(div, button);
-    			insert_dev(target, t19, anchor);
-    			if_block.m(target, anchor);
-    			insert_dev(target, t20, anchor);
-    			insert_dev(target, ul, anchor);
-
-    			for (let i = 0; i < each_blocks.length; i += 1) {
-    				each_blocks[i].m(ul, null);
-    			}
-
-    			if (!mounted) {
-    				dispose = [
-    					listen_dev(input, "input", /*input_input_handler*/ ctx[5]),
-    					listen_dev(button, "click", /*confirmPassword*/ ctx[3], false, false, false)
-    				];
-
-    				mounted = true;
-    			}
+    			insert_dev(target, header, anchor);
+    			append_dev(header, h1);
     		},
-    		p: function update(ctx, [dirty]) {
-    			if (dirty & /*enteredPassword*/ 1 && input.value !== /*enteredPassword*/ ctx[0]) {
-    				set_input_value(input, /*enteredPassword*/ ctx[0]);
-    			}
-
-    			if (current_block_type === (current_block_type = select_block_type(ctx)) && if_block) {
-    				if_block.p(ctx, dirty);
-    			} else {
-    				if_block.d(1);
-    				if_block = current_block_type(ctx);
-
-    				if (if_block) {
-    					if_block.c();
-    					if_block.m(t20.parentNode, t20);
-    				}
-    			}
-
-    			if (dirty & /*removePassword, passwords*/ 20) {
-    				each_value = /*passwords*/ ctx[2];
-    				validate_each_argument(each_value);
-    				let i;
-
-    				for (i = 0; i < each_value.length; i += 1) {
-    					const child_ctx = get_each_context(ctx, each_value, i);
-
-    					if (each_blocks[i]) {
-    						each_blocks[i].p(child_ctx, dirty);
-    					} else {
-    						each_blocks[i] = create_each_block(child_ctx);
-    						each_blocks[i].c();
-    						each_blocks[i].m(ul, null);
-    					}
-    				}
-
-    				for (; i < each_blocks.length; i += 1) {
-    					each_blocks[i].d(1);
-    				}
-
-    				each_blocks.length = each_value.length;
-    			}
-    		},
+    		p: noop,
     		i: noop,
     		o: noop,
     		d: function destroy(detaching) {
-    			if (detaching) detach_dev(h1);
-    			if (detaching) detach_dev(t1);
-    			if (detaching) detach_dev(p);
-    			if (detaching) detach_dev(t3);
-    			if (detaching) detach_dev(ol);
-    			if (detaching) detach_dev(t15);
-    			if (detaching) detach_dev(div);
-    			if (detaching) detach_dev(t19);
-    			if_block.d(detaching);
-    			if (detaching) detach_dev(t20);
-    			if (detaching) detach_dev(ul);
-    			destroy_each(each_blocks, detaching);
-    			mounted = false;
-    			run_all(dispose);
+    			if (detaching) detach_dev(header);
     		}
     	};
 
@@ -718,72 +338,16 @@ var app = (function () {
     	return block;
     }
 
-    function instance($$self, $$props, $$invalidate) {
+    function instance($$self, $$props) {
     	let { $$slots: slots = {}, $$scope } = $$props;
     	validate_slots("App", slots, []);
-    	let enteredPassword = " ";
-    	let passwordValidity = "short";
-    	let passwords = [];
-
-    	function confirmPassword() {
-    		if (passwordValidity === "good") {
-    			$$invalidate(2, passwords = [...passwords, enteredPassword]);
-    		}
-    	}
-
-    	function removePassword(index) {
-    		$$invalidate(2, passwords = passwords.filter((pw, idx) => {
-    			return idx !== index;
-    		}));
-    	}
-
     	const writable_props = [];
 
     	Object.keys($$props).forEach(key => {
     		if (!~writable_props.indexOf(key) && key.slice(0, 2) !== "$$") console.warn(`<App> was created with unknown prop '${key}'`);
     	});
 
-    	function input_input_handler() {
-    		enteredPassword = this.value;
-    		$$invalidate(0, enteredPassword);
-    	}
-
-    	$$self.$capture_state = () => ({
-    		enteredPassword,
-    		passwordValidity,
-    		passwords,
-    		confirmPassword,
-    		removePassword
-    	});
-
-    	$$self.$inject_state = $$props => {
-    		if ("enteredPassword" in $$props) $$invalidate(0, enteredPassword = $$props.enteredPassword);
-    		if ("passwordValidity" in $$props) $$invalidate(1, passwordValidity = $$props.passwordValidity);
-    		if ("passwords" in $$props) $$invalidate(2, passwords = $$props.passwords);
-    	};
-
-    	if ($$props && "$$inject" in $$props) {
-    		$$self.$inject_state($$props.$$inject);
-    	}
-
-    	$$self.$$.update = () => {
-    		if ($$self.$$.dirty & /*enteredPassword*/ 1) {
-    			 if (enteredPassword.trim().length < 5) {
-    				$$invalidate(1, passwordValidity = "short");
-    			} else if (enteredPassword.trim().length > 10) {
-    				$$invalidate(1, passwordValidity = "long");
-    			} else $$invalidate(1, passwordValidity = "good");
-    		}
-    	};
-
-    	return [
-    		enteredPassword,
-    		passwordValidity,
-    		passwords,
-    		confirmPassword,
-    		removePassword,
-    		input_input_handler
-    	];
+    	return [];
     }
 
     class App extends SvelteComponentDev {
--- src/App.svelte
+++ src/App.svelte
@@ -1,75 +1,14 @@
 <script>
-  let enteredPassword = " ";
-  let passwordValidity = "short";
-  let passwords = [];
-
-  $: if (enteredPassword.trim().length < 5) {
-    passwordValidity = "short";
-  } else if (enteredPassword.trim().length > 10) {
-    passwordValidity = "long";
-  } else passwordValidity = "good";
-
-  function confirmPassword() {
-    if (passwordValidity === "good") {
-      passwords = [...passwords, enteredPassword];
-    }
-  }
-  function removePassword(index) {
-    passwords = passwords.filter((pw, idx) => {
-      return idx !== index;
-    });
-  }
 </script>
 
 <style>
-  #form {
-    width: 30rem;
-    max-width: 100%;
+  header {
+    position: fixed;
+    width: 100%;
+    top: 0;
   }
 </style>
 
-<h1>Assignment</h1>
-
-<p>Solve these tasks.</p>
-
-<ol>
-  <li>
-    Add a enteredPassword input field and save the user input in a variable.
-  </li>
-  <li>
-    Output "Too short" if the password is shorter than 5 characters and "Too
-    long" if it's longer than 10.
-  </li>
-  <li>
-    Output the password in a paragraph tag (below the input or below the
-    warnings) if it's between these boundaries.
-  </li>
-  <li>
-    Add a button and let the user add the passwords to an array (an array that
-    grows with every password that was entered - not too long not too short).
-  </li>
-  <li>Output the array values (= passwords) in a unordered list (ul tag).</li>
-  <li>Bonus: If a password is clicked, remove it from the list.</li>
-</ol>
-
-<div class="form">
-  <h3>Password</h3>
-  <input type="text" bind:value={enteredPassword} /><button
-    on:click={confirmPassword}>Confirm Password
-  </button>
-</div>
-
-<!-- password input field validation -->
-{#if passwordValidity === 'long'}
-  <p>Password is too long</p>
-{:else if passwordValidity === 'short'}
-  <p>Password is too short</p>
-{:else}
-  <p>Password: {enteredPassword}</p>
-{/if}
-
-<ul>
-  {#each passwords as pw, i}
-    <li on:click={removePassword.bind(this, i)}>{pw}</li>
-  {/each}
-</ul>
+<header>
+  <h1>Meet Ups</h1>
+</header>
