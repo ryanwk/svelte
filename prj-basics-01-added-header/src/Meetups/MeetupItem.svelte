9b5f3d39e00e57e66f1ce0860990e1ea4fd09c60
ryanwk <ryanwkoch@gmail.com>
2020-11-30 16:46:44 -0500

    created buttons and Title, Subtitle in H tags of MeetupItem.svelte

--- prj-basics-01-added-header/src/Meetups/MeetupItem.svelte
+++ prj-basics-01-added-header/src/Meetups/MeetupItem.svelte
@@ -1,3 +1,10 @@
 <article>
   <header />
+  <h1>TITLE</h1>
+  <h2>SUBTITLE</h2>
+  <div class="Image"><img src="" alt="" /></div>
+  <div class="content">
+    <p />
+  </div>
+  <footer><button>Show Details</button><button>Favorite</button></footer>
 </article>
