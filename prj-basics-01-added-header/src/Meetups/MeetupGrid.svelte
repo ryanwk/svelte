69ad690f8a948bc56839745ea7ed3c402dbfc54c
ryanwk <ryanwkoch@gmail.com>
2020-12-03 10:00:15 -0500

    added style tag with css to format the grid

--- prj-basics-01-added-header/src/Meetups/MeetupGrid.svelte
+++ prj-basics-01-added-header/src/Meetups/MeetupGrid.svelte
@@ -4,6 +4,20 @@
   export let meetups;
 </script>
 
+<style>
+  section {
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-gap: 1rem;
+  }
+  @media (min-width: 768px) {
+    section {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+</style>
+
 <section id="meetups">
   {#each meetups as meetup}
     <MeetupItem
