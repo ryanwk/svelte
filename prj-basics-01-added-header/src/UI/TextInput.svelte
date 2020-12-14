52e678b87178953fa0655a4b513061a0b67a5263
ryanwk <ryanwkoch@gmail.com>
2020-12-14 15:40:44 -0500

    created props id, label, rows, value to pass into and set corresponding variables within the custom textinput component if block

--- prj-basics-01-added-header/src/UI/TextInput.svelte
+++ prj-basics-01-added-header/src/UI/TextInput.svelte
@@ -1,14 +1,14 @@
 <script>
   export let controlType;
+  export let id;
+  export let label;
+  export let rows;
+  export let value;
 </script>
 
 <div class="form-control">
-  <label for="description">Description</label>
-{#if controlType === 'textarea'}
-  <textarea rows="3" id="description" bind:value={description} />
-
-{:else}
-  <input type="text" id="title" bind:value={title} />
-</div>
-{/if}
+  <label for={id}>label</label>
+  {#if controlType === 'textarea'}
+    <textarea {rows} {id} vaue={value} on:input />
+  {:else}<input type="text" {id} bind:value={label} />{/if}
 </div>
