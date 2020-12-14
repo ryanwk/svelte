caa1d61e66fc38f8381308ad237b7547613b61e4
ryanwk <ryanwkoch@gmail.com>
2020-12-14 16:19:02 -0500

    TextInput component used to replace email, description components
    added a type variable to set each TextInput accordingly
    
    added controlType of textarea to description as this will return true in the TextInput if else block

--- prj-basics-01-added-header/src/App.svelte
+++ prj-basics-01-added-header/src/App.svelte
@@ -61,37 +61,44 @@
     <TextInput
       id="title"
       label="Title"
+      type="text"
       value={title}
       on:input={(event) => (title = event.target.value)} />
 
     <TextInput
       id="subtitle"
       label="Subtitle"
+      type="text"
       value={subtitle}
       on:input={(event) => (subtitle = event.target.value)} />
 
     <TextInput
       id="address"
       label="Address"
+      type="text"
       value={address}
       on:input={(event) => (address = event.target.value)} />
 
     <TextInput
       id="imageUrl"
       label="Image Url"
+      type="image"
       value={imageUrl}
       on:input={(event) => (imageUrl = event.target.value)} />
 
     <TextInput
       id="email"
       label="E-mail"
+      type="email"
       value={email}
       on:input={(event) => (email = event.target.value)} />
 
-    <div class="form-control">
-      <label for="description">Description</label>
-      <textarea rows="3" id="description" bind:value={description} />
-    </div>
+    <TextInput
+      id="description"
+      label="Description"
+      controlType="textarea"
+      value={description}
+      on:input={(event) => (description = event.target.value)} />
     <button type="submit">Save</button>
   </form>
   <MeetupGrid {meetups} />
