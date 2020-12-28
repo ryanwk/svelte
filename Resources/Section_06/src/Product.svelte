885bae09dc6e8af0c96eb2f5c07ef66d42b1eb29
ryanwk <ryanwkoch@gmail.com>
2020-12-28 14:39:46 -0500

    added comment detailing event forwarding above the Add to Cart button

--- Resources/Section_06/src/Product.svelte
+++ Resources/Section_06/src/Product.svelte
@@ -7,6 +7,7 @@
 
 <article>
     <h1>{productTitle}</h1>
+    <!-- events don't propogate across event boundaries so you need to register the event listener on each button without an assignment-->
     <button on:click>Add to Cart</button>
     <button>Delete</button>
 </article>
