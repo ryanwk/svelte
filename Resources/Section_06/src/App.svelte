f39126f4ab8272d892e0607f0b66d8695e65b398
ryanwk <ryanwkoch@gmail.com>
2020-12-28 15:06:48 -0500

    using functions addToCart and deleteProduct, along with console logs, I am able to test the data that is passed to the Product component

--- Resources/Section_06/src/App.svelte
+++ Resources/Section_06/src/App.svelte
@@ -1,5 +1,13 @@
 <script>
 	import Product from "./Product.svelte";
+
+	function addToCart(event) {
+		console.log(event.detail);
+	}
+
+	function deleteProduct(event) {
+		console.log(event.detail);
+	}
 </script>
 
 <!--<style>
@@ -12,7 +20,5 @@
 <!-- emitting custom events  -->
 <Product
 	productTitle="A Book"
-	on:add-to-cart={() => alert('Add to cart!')}
-	on:delete={() => {
-		alert('Delete!');
-	}} />
+	on:add-to-cart={addToCart}
+	on:delete={deleteProduct} />
