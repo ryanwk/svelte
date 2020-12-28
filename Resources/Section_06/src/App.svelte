0f78ae22f822a22ad22a267b9017d45d649bb783
ryanwk <ryanwkoch@gmail.com>
2020-12-28 14:53:54 -0500

    App.svelte: added comment detailing event forwarding above the Add to Cart button
    Product.svelete: imported createEventDispatcher from svelte and created function addToCart to handle emitting the custom event whereas the Delete button has the same functionality hardcoded inline. The reason these must be separated is because events don't propogate across event boundaries

--- Resources/Section_06/src/App.svelte
+++ Resources/Section_06/src/App.svelte
@@ -4,5 +4,15 @@
 
 <!--<style>
 </style>-->
+
+<!-- event forwarding and emitting custom events  -->
 <!-- throw an alert when user hits Delete or Add to Cart using forwarding -->
-<Product productTitle="A Book" on:click={() => alert('Clicked')} />
+<!-- <Product productTitle="A Book" on:click={() => alert('Clicked')} /> -->
+
+<!-- emitting custom events  -->
+<Product
+	productTitle="A Book"
+	on:add-to-cart={() => alert('Add to cart!')}
+	on:delete={() => {
+		alert('Delete!');
+	}} />
--- Resources/Section_06/src/Product.svelte
+++ Resources/Section_06/src/Product.svelte
@@ -1,5 +1,16 @@
 <script>
+    // allows you to create event dispatching statement
+    import { createEventDispatcher } from "svelte";
+
     export let productTitle;
+
+    const dispatch = createEventDispatcher();
+
+    // this function invokes createEventDispatcher via the dispatch function
+    function addToCart() {
+        // takes name of event that should be listened to
+        dispatch("add-to-cart", { id: "p1" });
+    }
 </script>
 
 <style>
@@ -8,6 +19,9 @@
 <article>
     <h1>{productTitle}</h1>
     <!-- events don't propogate across event boundaries so you need to register the event listener on each button without an assignment-->
-    <button on:click>Add to Cart</button>
-    <button>Delete</button>
+    <button on:click={addToCart}>Add to Cart</button>
+    <button
+        on:click={() => {
+            dispatch('delete', 'p1');
+        }}>Delete</button>
 </article>
