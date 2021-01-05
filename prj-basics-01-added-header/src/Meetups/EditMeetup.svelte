ddfcb67c4a9538b3d9ecf92e95db9584b2efd0a5
ryanwk <ryanwkoch@gmail.com>
2021-01-05 14:30:37 -0500

    TextInput component has an if check for error handling
    if a field is not valid and there is a validityMessage then custom css is applied to a paragraph with the validityMessage displayed

--- prj-basics-01-added-header/src/Meetups/EditMeetup.svelte
+++ prj-basics-01-added-header/src/Meetups/EditMeetup.svelte
@@ -20,12 +20,12 @@
       address: address,
       email: email,
       description: description,
-      imageUrl: imageUrl
+      imageUrl: imageUrl,
     });
   }
 
   function cancel() {
-      dispatch('cancel');
+    dispatch("cancel");
   }
 </script>
 
@@ -40,35 +40,37 @@
     <TextInput
       id="title"
       label="Title"
+      valid={true}
+      validityMessage={true}
       value={title}
-      on:input={event => (title = event.target.value)} />
+      on:input={(event) => (title = event.target.value)} />
     <TextInput
       id="subtitle"
       label="Subtitle"
       value={subtitle}
-      on:input={event => (subtitle = event.target.value)} />
+      on:input={(event) => (subtitle = event.target.value)} />
     <TextInput
       id="address"
       label="Address"
       value={address}
-      on:input={event => (address = event.target.value)} />
+      on:input={(event) => (address = event.target.value)} />
     <TextInput
       id="imageUrl"
       label="Image URL"
       value={imageUrl}
-      on:input={event => (imageUrl = event.target.value)} />
+      on:input={(event) => (imageUrl = event.target.value)} />
     <TextInput
       id="email"
       label="E-Mail"
       type="email"
       value={email}
-      on:input={event => (email = event.target.value)} />
+      on:input={(event) => (email = event.target.value)} />
     <TextInput
       id="description"
       label="Description"
       controlType="textarea"
       value={description}
-      on:input={event => (description = event.target.value)} />
+      on:input={(event) => (description = event.target.value)} />
   </form>
   <div slot="footer">
     <Button type="button" mode="outline" on:click={cancel}>Cancel</Button>
